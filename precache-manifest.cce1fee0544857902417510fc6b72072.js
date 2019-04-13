d3514b88515555ee629ce83a6575e5c0aefeaebd
KevinHerlt <kevin.herlt1998@gmail.com>
2019-04-13 16:56:51 +0200

    Updates

---
+++ precache-manifest.cce1fee0544857902417510fc6b72072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a03dc12cda14a16444a4",
+    "url": "/test/static/js/runtime~main.63e4a3bf.js"
+  },
+  {
+    "revision": "1fbb00b460aa70039021",
+    "url": "/test/static/js/main.0dbce62e.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/test/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "1fbb00b460aa70039021",
+    "url": "/test/static/css/main.54552c51.chunk.css"
+  },
+  {
+    "revision": "c6c8da67d285b415f60a9e814f551f89",
+    "url": "/test/index.html"
+  }
+];
